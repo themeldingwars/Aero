13b465974701a3f4d061ee671974dc801f3f632c
Szymon Kamiński <kaminskisj@gmail.com>
2025-05-01 13:45:06 +0200

    Allow AeroSdb on more targets

--- Aero/Aero.Gen/Attributes/AeroSDBAttribute.cs
+++ Aero/Aero.Gen/Attributes/AeroSDBAttribute.cs
@@ -3,7 +3,7 @@
 namespace Aero.Gen.Attributes
 {
     // Used to mark a field as containing a value in the SDB
-    [AttributeUsage(AttributeTargets.Field)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = true)]
     public class AeroSdbAttribute : Attribute
     {
         public static string Name = "AeroSdb";
